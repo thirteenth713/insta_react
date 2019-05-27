7997e90c6685ed2fe57f4edae30538dd9c1b11b9
Thirteenth <thirteenth713@gmail.com>
2019-05-27 22:35:55 +0300

    create services and methods for work with server, install JSON server

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import Post from './Post';
+import InstaService from '../services/instaservice';
 
 export default class Posts extends Component {
-  render () {
+    //Class fields ES10
+    InstaService = new InstaService();
+    state = {
+        posts: [],
+        error: false
+    };
+
+    componentDidMount() {
+        this.updatePosts();
+    }
+
+    updatePosts() {
+        this.InstaService.getAllPosts()
+            .then(this.onPostsLoaded)
+            .catch()
+    }
+
+    render () {
     return (
       <div className="left">
         <Post alt="nature" src="http://www.youandthemat.com/wp-content/uploads/nature-2-26-17.jpg" />
